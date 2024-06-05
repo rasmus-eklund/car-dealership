20dfa7241b9bac4f6ddc8cfe415529da34a6e78f
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-06-05 16:45:04 +0200

    fix slash

--- car_dealership/cars/urls.py
+++ car_dealership/cars/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:car_id>/', views.car_detail, name='car'),
+    path('<int:car_id>', views.car_detail, name='car'),
     path('form/<str:form_type>/', views.admin_forms, name='admin_forms'),
     path('edit/<int:car_id>/', views.edit_car, name='edit_car'),
+    path('delete/<int:car_id>/', views.delete_car, name='delete_car'),
     path('car/delete_image/<int:image_id>/',
          views.delete_car_image, name='delete_car_image'),
     path('add_car/', views.add_car, name='add_car'),
