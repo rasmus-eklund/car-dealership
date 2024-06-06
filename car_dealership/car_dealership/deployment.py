7a7455fa8e03289f8f2f55e966155d7bc00d4089
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-06-06 06:52:57 +0200

    debug true

--- car_dealership/car_dealership/deployment.py
+++ car_dealership/car_dealership/deployment.py
@@ -6,7 +6,7 @@ from azure.identity import DefaultAzureCredential
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 
-DEBUG = False
+DEBUG = True
 SECRET_KEY = os.environ['MY_SECRET_KEY']
 
 MIDDLEWARE = [
